f16a19a5de5de9921cc9711870a66fbda85f66fe
Mikalai Dzerachyts <mikalai.dzerachyts@travisperkins.co.uk>
2014-09-01 13:42:56 +0300

    Html Title was added

--- src/test/java/pawn/webapp/HelloControllerWebAppTest.java
+++ src/test/java/pawn/webapp/HelloControllerWebAppTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -39,7 +40,7 @@ public class HelloControllerWebAppTest {
     public void testGreeting() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World!"));
+                .andExpect(content().string( containsString("Hello World!")));
     }
 
 }
