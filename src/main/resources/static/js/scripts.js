2c8884776ceaf7e771cbdd6497346dfc80c2cdb2
nderachits <n.derachits@gmail.com>
2015-02-11 00:49:27 +0200

    target pawn is removed on attack

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -6,17 +6,44 @@ function allowDrop(ev) {
     ev.preventDefault();
 }
 
+function allowDropToImage(ev) {
+    ev.preventDefault();
+}
+
+function getCssProperty(elmId, property){
+    var elem = document.getElementById(elmId);
+    return window.getComputedStyle(elem,null).getPropertyValue(property);
+}
+
+function dropToImage(ev) {
+    console.log("drop img: "+ev.target.id);
+    var data = ev.dataTransfer.getData("text");
+
+    if(ev.target.id === data) {
+        return;
+    }
+
+    var x = document.querySelectorAll("#"+data);
+    x[0].style.top = getCssProperty(ev.target.id, "top");
+    x[0].style.left = getCssProperty(ev.target.id, "left");
+
+    clearPawn(ev.target);
+}
+
 function drag(ev) {
+    console.log("moving img: "+ev.target.id);
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    var cellId = ev.target.getAttribute('id').substring("cell".length) ;
+    console.log("move img: "+ev.target.id+", data: "+data);
+    var cellId = ev.target.id.substring("cell".length) ;
+    console.log("cellId: "+cellId);
     var x = document.querySelectorAll("#"+data);
     x[0].style.top = ""+calcTopFromId(cellId)+"px";
-    x[0].style.left = ""+calcLeftFromId(cellId)+"px";;
+    x[0].style.left = ""+calcLeftFromId(cellId)+"px";
 }
 
 function calcTopFromId(id) {
@@ -28,5 +55,6 @@ function calcLeftFromId(id) {
 }
 
 function clearPawn(el) {
+    console.log("clear img: "+el.id);
     el.style.visibility = "hidden";
 }
