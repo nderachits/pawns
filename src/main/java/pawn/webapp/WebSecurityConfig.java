25c42c8b2dfd8265cb08db03f5d190dbf4a48c05
nderachits <n.derachits@gmail.com>
2015-04-03 15:43:10 +0300

    UserDetailsManager is exposed

--- src/main/java/pawn/webapp/WebSecurityConfig.java
+++ src/main/java/pawn/webapp/WebSecurityConfig.java
@@ -1,11 +1,20 @@
 package pawn.webapp;
 
+import java.util.Arrays;
+import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 /**
  * Created by Mikalai_Dzerachyts on 4/1/2015.
@@ -13,6 +22,8 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @Configuration
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsManager userDetailsManager;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,10 +35,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin();
     }
 
+    @Bean
+    public UserDetailsManager getUserDetailsManager() {
+        return userDetailsManager;
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        auth
-                .inMemoryAuthentication()
-                    .withUser("user").password("password").roles("USER");
+//        auth
+//                .inMemoryAuthentication()
+//                    .withUser("user").password("password").roles("USER");
+
+        userDetailsManager = new InMemoryUserDetailsManager(Collections.<UserDetails>emptyList());
+        UserDetails user = new User("user", "password", Arrays.<GrantedAuthority>asList(new SimpleGrantedAuthority("USER")));
+        System.out.println("Config Spring Security");
+        userDetailsManager.createUser(user);
     }
 }
